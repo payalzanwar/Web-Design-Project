4d2d9a9e30d996fce3925b7eece9b357c9fee890
Saurabh Nitin Gujare <gujare.s@husky.neu.edu>
2019-03-31 16:58:27 -0400

    [code] added Jumbotron in About US page

--- src/app/about-us/about-us.module.ts
+++ src/app/about-us/about-us.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutUsHeaderImageComponent } from './about-us-header-image/about-us-header-image.component';
+import { AboutUsJumbotronComponent } from './about-us-jumbotron/about-us-jumbotron.component';
 
 @NgModule({
-  declarations: [AboutUsHeaderImageComponent],
+  declarations: [AboutUsHeaderImageComponent, AboutUsJumbotronComponent],
   imports: [
     CommonModule
   ],
-  exports:[AboutUsHeaderImageComponent]
+  exports:[
+    AboutUsHeaderImageComponent,
+    AboutUsJumbotronComponent
+  ]
 })
 export class AboutUsModule { }
