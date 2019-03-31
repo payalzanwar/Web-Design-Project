20769aed21a2be156e7de4d1b92227fdbedaa7e9
Saurabh Nitin Gujare <gujare.s@husky.neu.edu>
2019-03-31 15:37:32 -0400

    [Code]Added about-us module and about-us-header-image component

---
+++ src/app/about-us/about-us.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutUsHeaderImageComponent } from './about-us-header-image/about-us-header-image.component';
+
+@NgModule({
+  declarations: [AboutUsHeaderImageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AboutUsModule { }
