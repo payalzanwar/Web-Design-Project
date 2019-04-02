cf4d9be0d13b3e80fba2f0b28618c1106d92899b
Saurabh Nitin Gujare <gujare.s@husky.neu.edu>
2019-04-02 00:19:02 -0400

    [UI] Added Signedin Navbar and Signedout Navbar components

--- src/app/header/navbar-signedin/navbar-signedin.component.spec.ts
+++ src/app/header/navbar-signedin/navbar-signedin.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavBarComponent } from './nav-bar.component';
+import { NavbarSignedinComponent } from './navbar-signedin.component';
 
-describe('NavBarComponent', () => {
-  let component: NavBarComponent;
-  let fixture: ComponentFixture<NavBarComponent>;
+describe('NavbarSignedinComponent', () => {
+  let component: NavbarSignedinComponent;
+  let fixture: ComponentFixture<NavbarSignedinComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavBarComponent ]
+      declarations: [ NavbarSignedinComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavBarComponent);
+    fixture = TestBed.createComponent(NavbarSignedinComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
