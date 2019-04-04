517fdac1f03f89f8382a25f2dcff30895e88ac81
Saurabh Nitin Gujare <gujare.s@husky.neu.edu>
2019-04-04 01:53:13 -0400

    [UI] Changes in footer components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutUsHeaderImageComponent } from './about-us/about-us-header-image/about-us-header-image.component';
 import { CardPackageComponent } from './package-page/card-package/card-package.component';
+import { FooterComponent } from './footer/footer/footer.component';
 
 const routes: Routes = [
   {path:'aboutus',component:AboutUsHeaderImageComponent},
-  {path:'packagelist',component:CardPackageComponent}
+  {path:'packagelist',component:CardPackageComponent},
+  {path:'footer',component:FooterComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutUsModule } from './about-us/about-us.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderModule } from './header/header.module';
 import { PackagePageModule } from './package-page/package-page.module';
+import { FooterModule } from './footer/footer.module';
 
 @NgModule({
   declarations: [
@@ -14,10 +15,12 @@ import { PackagePageModule } from './package-page/package-page.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     AboutUsModule,
     HeaderModule,
     PackagePageModule,
+    FooterModule,
     NgbModule.forRoot()
   ],
   providers: [],
