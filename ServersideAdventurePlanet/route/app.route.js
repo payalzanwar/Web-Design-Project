3749acb6c604bf88ded5c360cb215d0b9ca3a894
gayathriThammana <thammana.g@husky.neu.edu>
2019-04-16 14:54:19 -0400

    Saving Sites data to DB and rendering including images

--- ServersideAdventurePlanet/route/app.route.js
+++ ServersideAdventurePlanet/route/app.route.js
@@ -1,7 +1,16 @@
 module.exports = (app) => {
     const user = require('../controller/userController');
+    const transportSiteController = require('../controller/transportSitesController');
+    const accomodationSitesCtrl = require('../controller/accomodationSitesController');
 
     app.post('/user',user.create);
     app.get('/user/:email/:password',user.getUser);
     app.delete('/user/:email',user.delete);
+
+    app.get('/transportSites/:transportType', transportSiteController.getSites);
+    app.post('/transportSite/create', transportSiteController.addSites);
+
+    app.get('/accomodationSites', accomodationSitesCtrl.getSites);
+    app.post('/accomodationSite/create', accomodationSitesCtrl.addSites);
+
 }
